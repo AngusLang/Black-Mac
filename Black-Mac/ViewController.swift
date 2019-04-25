0c366437b7f4afec109f10d49934589f605c9ed1
angus <lang@zjxu.org>
2019-04-25 14:31:28 +0800

    remove obj loader

--- Black-Mac/ViewController.swift
+++ Black-Mac/ViewController.swift
@@ -25,22 +25,7 @@ class ViewController: NSViewController {
 
         let standardPass = StandardPass(renderer: renderer!)
         renderer?.passes.append(standardPass)
-        
-        let monkeyURI = Bundle.main.path(forResource: "monkey", ofType: "obj")! as String
-        let basePath = (monkeyURI as NSString).deletingLastPathComponent
-        let content = try! String.init(contentsOfFile: monkeyURI, encoding: .utf8)
-        let loader = OBJLoader(source: content, basePath: basePath as NSString)
-        let shapes = try! loader.read()
-        print(shapes)
-        
-//        let loader OBJScannerer()
-//
-//        let geo = loader.load(url: monkeyURI!)
-//        let materialMap = Bundle.main.path(forResource: "skin", ofType: "jpg")
-//        let mat = Material()
-//        mat.map = materialMap
-//        let monkey = RenderNode(geometry: geo, material: mat)
-//        standardPass.addRenderNode(monkey)
+    
     }
 
 }
